1257a18e82598f52918e69250c071ce4518326d9
PsyHye <griffinlochner@gmail.com>
2022-10-04 12:45:10 -0500

    begin resume content and styling

--- scripts/main.js
+++ scripts/main.js
@@ -18,7 +18,41 @@ function navAbout() {
 
 function navResume() {
   main.className = "resume-text"
-  main.innerHTML = "<h2>Resume</h2><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p><p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.</p><p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>";
+  main.innerHTML = 
+  `
+  <h2>Resume</h2>
+  <br>
+  <h3>Widen Enterprises / Acquia</h3> <time>June 2009 - Present</time> <span> Madison, WI</span>
+  <br><br>
+  <h4>Senior Software Test Engineer</h4><time>September 2012 - Present</time>
+  <ul>
+  <li>Write, manage, and refactor test automation suites to support continuous integration of new
+  product features and identify issues</li>
+  <li>Develop and execute test plans for new and existing functionality</li>
+  <li>Perform code reviews for developers and other test engineers</li>
+  <li>Create detailed reports that identify underlying causes of issues</li>
+  <li>Work with product managers, tech leads, and developers to ensure the quality of products</li>
+  <li>Provide visibility, transparency and accountability to testing process and the state of testing</li>
+  </ul>
+  <br>
+  <h4>Software Technical Support</h4> <time>June 2009 - September 2012</time>
+  <ul>
+  <li>Provide technical support via email, chat, and phone for a suite of marketing technology
+  applications</li>
+  <li>Communicate best practices with client administrators to ensure their successful use and
+  engagement of various application features</li>
+  <li>Assist with the planning and configuration of new client Digital Asset Management SaaS</li>
+  </ul>
+  <br><br>
+  <h3>Esker Software</h3> <time>June 2007 - June 2009</time> <span>Madison, WI</span>
+  <br><br>
+  <h4>Client Support Specialist</h4>
+  <ul>
+  <li>Lead technical support triage responsible for qualifying client software issue severity,
+  troubleshooting, and providing timely resolution to live issues</li>
+  <li>Process client requests for account setup, product upgrades, and professional services</li>
+  </ul>
+  `;
   header.className = "header-closed";
   document.title = "Griffin Lochner - Resume";
 }
