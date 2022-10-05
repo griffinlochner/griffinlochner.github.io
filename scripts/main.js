5476e38bc7588df2cf14528190ad62fe59fcad61
PsyHye <griffinlochner@gmail.com>
2022-10-05 15:12:30 -0500

    style the scrollbar

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,37 @@ function toggleNav() {
 
 function navAbout() {
   main.className = "about-text"
-  main.innerHTML = "<h2>About</h2><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p><p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.</p><p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>";
+  main.innerHTML = `
+  <h2>About</h2>
+  <p>* under construction *</p>
+  <p>
+  professional career
+  purpose of this website
+  </p>
+  <p>
+  basic info
+  family/dogs
+  madison WI entertainment
+  </p>
+  <p>
+  passion for learning
+  reading
+  </p>
+  <p>
+  passion for gaming
+  mtg
+  nintendo
+  etc.
+  </p>
+  <p>
+  passion for art
+  different styles, painting digital, anime, psychedelic, landscapes
+  </p>
+  <p>
+  passion for music
+  different styles, psychedelic trance, phonk, heavy metal
+  </p>
+  `;
   header.className = "header-closed";
   document.title = "Griffin Lochner - About";
 }
