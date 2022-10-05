efb2425e904b42a7612a1fb6963009e423a5ceaf
PsyHye <griffinlochner@gmail.com>
2022-10-05 12:27:23 -0500

    finish start of books page

--- scripts/main.js
+++ scripts/main.js
@@ -94,14 +94,40 @@ function navResume() {
 
 function navArt() {
   main.className = "art-text"
-  main.innerHTML = "<h2>Eventually various art I've made will be here...</h2><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p><p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.</p><p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>";
+  main.innerHTML = `
+  <h2>Eventually various art I've made will be here...</h2>
+  `;
   header.className = "header-closed";
   document.title = "Griffin Lochner - Art";
 }
 
 function navBooks() {
   main.className = "books-text"
-  main.innerHTML = "<h2>Eventually books I've read and recommend will be here...</h2><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p><p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.</p><p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>";
+  main.innerHTML = `
+  <h2>Recommended Reading</h2>
+  <ul>
+  <li>A Brief History of Time <span>Stephen Hawking</span></li>
+  <li>Arguably <span>Christopher Hitchens</span></li>
+  <li>Billions & Billions <span>Carl Sagan</span></li>
+  <li>Brief Answers to the Big Questions <span>Stephen Hawking</span></li>
+  <li>Broca's Brain <span>Carl Sagan</span></li>
+  <li>Cosmos <span>Carl Sagan</span></li>
+  <li>Free Will <span>Sam Harris</span></li>
+  <li>How to Change Your Mind <span>Michael Pollan</span></li>
+  <li>How to Create a Mind <span>Ray Kurzweil</span></li>
+  <li>Hyperspace <span>Michio Kaku</span></li>
+  <li>In Defense of Food <span>Michael Pollan</span></li>
+  <li>Lying <span>Sam Harris</span></li>
+  <li>Pale Blue Dot <span>Carl Sagan</span></li>
+  <li>Physics of the Future <span>Michio Kaku</span></li>
+  <li>Shadows of Forgotten Ancestors <span>Ann Druyan, Carl Sagan</span></li>
+  <li>Superintelligence <span>Nick Bostrom</span></li>
+  <li>The Age of Spiritual Machines <span>Ray Kurzweil</span></li>
+  <li>The Demon Haunted World <span>Carl Sagan</span></li>
+  <li>The Dragons of Eden <span>Carl Sagan</span></li>
+  <li>This is Your Mind on Plants <span>Michael Pollan</span></li>
+
+  </ul>`;
   header.className = "header-closed";
   document.title = "Griffin Lochner - Books";
 }
